65eb3ec7438b183700b93f137687a8641bd0d51b
colin <colin@ColinWebServer>
2024-08-29 09:42:59 +0100

    29/08/2024 09:11 copy over Notification.java from 1.43d branch

---
+++ src/main/java/view/views/non_modal_views/PatientNotificationView.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view.views.non_modal_views;
+
+/**
+ *
+ * @author colin
+ */
+public class PatientNotificationView {
+    
+}
